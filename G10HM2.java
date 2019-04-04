ec7c664ceab9de5ffb1797334883c000054f9bcb
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-04-04 17:05:41 +0200

    Update G10HM2.java

--- G10HM2.java
+++ G10HM2.java
@@ -1,24 +1,30 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaPairRDD;//add this import for the JavaPair
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import java.util.HashMap;//add for the HashMap
+
+import java.io.IOException;
+import java.util.HashMap;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Map;// add for Map
-import scala.Tuple2;// add for Tuple2
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import scala.Tuple2;
+
+import static org.apache.spark.api.java.JavaRDDLike$class.mapPartitionsToPair;
 
 public class G10HM2 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         if (args.length == 0) {
             throw new IllegalArgumentException("Expecting the file name on the command line");
         }
 
         //Gets the number of partition, which is given in input
-        int k = Integer.parseInt(args[0]);
+        int k = 35; //[args[0]] ?
 
         // Setup Spark
         SparkConf conf = new SparkConf(true)
@@ -27,7 +33,6 @@ public class G10HM2 {
 
         /*
         Reads the collection of documents into an RDD docs.
-
         [ Note that if a path to a directory rather than to
         a file is passed to textFile, it will load all files
         found in the directory into the RDD. ]
@@ -35,7 +40,7 @@ public class G10HM2 {
         JavaRDD<String> docs = sc.textFile("filepath").cache();
         //Subdivides the collection into K partitions;
         docs.repartition(k);
-        
+
         /*
         We want to exclude the time to load the text file
         from our measurements. To do so we need to force the
@@ -44,14 +49,15 @@ public class G10HM2 {
         */
         docs.count();
 
-        
+
 
         //IMPROVED WORDCOUNT 1________________________________________________________
 
         long start1 = System.currentTimeMillis();
 
         JavaPairRDD<String, Long> wordcountpairs = docs
-                // Map phase
+
+                // Map phase.
                 .flatMapToPair((document) -> {
                     String[] tokens = document.split(" ");
                     HashMap<String, Long> counts = new HashMap<>();
@@ -66,18 +72,26 @@ public class G10HM2 {
                     return pairs.iterator();
                 })
 
-                // Reduce phase with reduceByKey method, with this method the word count is done in 2187ms
+                /*
+                Reduce phase.
+                With reduceByKey() method, the word count is computed in 2187ms
+                */
+
                 .reduceByKey((x,y)->x+y);
-                
-                //Reduce phase with groupByKey method, this method take 3271ms
-                //.groupByKey()
-                //.mapValues((it) -> { //this method requires also a mapValues
-                //long sum = 0;
-                //for (long c : it) {
-                //  sum += c;
-                //}
-                //return sum;
-                //});
+
+                /*
+
+                [Reduce phase with groupByKey() method, takes 3271ms]
+
+                .groupByKey()
+                .mapValues((it) -> { //this method requires also a mapValues
+                long sum = 0;
+                for (long c : it) {
+                sum += c;
+                }
+                return sum;
+                });
+                */
 
         System.out.println("Improved Word Count 1 found: " + wordcountpairs.count() + " unique words");
         long end1 = System.currentTimeMillis();
@@ -86,6 +100,56 @@ public class G10HM2 {
 
 
 
+        //IMPROVED WORDCOUNT 2.1________________________________________________________
+
+        long start2 = System.currentTimeMillis();
+
+        //Map_1
+        JavaPairRDD<String, Long> word_count_pairs = docs
+
+                .flatMapToPair((document) -> {
+                    String[] tokens = document.split(" ");
+                    HashMap<String, Long> counts = new HashMap<>();
+                    ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+
+                    for (String token : tokens) {
+                        counts.put(token, 1L + counts.getOrDefault(token, 0L));
+                    }
+                    for (Map.Entry<String, Long> e : counts.entrySet()) {
+                        pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+                    return pairs.iterator();
+
+                })
+                .groupBy(it -> ThreadLocalRandom.current().nextInt(0, k))
+
+        //Reduce_1
+                .mapValues(x -> x._2);
+
+                    /*
+                    for (String token : tokens) {
+
+                        counts.put([LONG]token, 1L + counts.getOrDefault(token, 0L));
+                    }
+                    for (Map.Entry<Long, Tuple2<String, Long>> e : counts.entrySet()) {
+
+                        pairs.add((int) ThreadLocalRandom.current().nextLong(sqrtN), new Tuple2<>(e.getKey(), e.getValue()));
+                    }
+
+                    return pairs.iterator();
+                });
+                    */
+
+        //Reduce_1
+
+
+
+
+        System.out.println("Improved Word Count 1 found: " + wordcountpairs.count() + " different words");
+        long end2 = System.currentTimeMillis();
+        System.out.println("Elapsed time " + (end2 - start2) + " ms");
+        //_______________________________________________________________________________
+
 
 
         //The following lines have to be the last part of the main method
@@ -93,3 +157,4 @@ public class G10HM2 {
         System.in.read();
     }
 }
+

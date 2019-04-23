33813df184cf8c9473de43f8b7ffac721907badd
Signorcarta <46000153+signorcarta@users.noreply.github.com>
2019-04-23 18:51:32 +0200

    Update G10HM3.java

--- G10HM3.java
+++ G10HM3.java
@@ -1,5 +1,5 @@
-import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class G10HM3 {
-
+    //__________________________________________________________________________________________________________________
     public static Vector strToVector(String str) {
         String[] tokens = str.split(" ");
         double[] data = new double[tokens.length];
@@ -17,7 +17,10 @@ public class G10HM3 {
         }
         return (Vector) Vectors.dense(data);
     }
+    //__________________________________________________________________________________________________________________
 
+
+    //__________________________________________________________________________________________________________________
     public static ArrayList<Vector> readVectorsSeq(String filename) throws IOException {
         if (Files.isDirectory(Paths.get(filename))) {
             throw new IllegalArgumentException("readVectorsSeq is meant to read a single file.");
@@ -28,7 +31,10 @@ public class G10HM3 {
                 .forEach(e -> result.add(e));
         return result;
     }
+    //__________________________________________________________________________________________________________________
 
+
+    //__________________________________________________________________________________________________________________
     public static ArrayList<Vector> kmeansPP( ArrayList<Vector> Pi, ArrayList<Long> WPi, int k, int iter){
 
         //Initializations
@@ -38,17 +44,21 @@ public class G10HM3 {
         ArrayList<Double> curMinDist = new ArrayList<>();
 
         double distSum = 0;
+        double randNum;
+        double[] probDist;
 
-        //Generate random number n
+        long objFunction;
+
+        //Generate random number n_____________________________________________________________________
         Random rand = new Random();
         int n = rand.nextInt(P.size());
-
         //Random point chosen from P
-        C.add(P.get(n));
+        C.add(P.get(n)); //First center selected
         P.remove(n); //remove it from the initial set
         WP.remove(n);
+        //_____________________________________________________________________________________________
 
-        //Main For cycle________________________________________________________________________________________________
+        //Main For cycle_______________________________________________________________________________
         for(int i=1; i<k; k++){
 
             distSum = 0;
@@ -66,14 +76,52 @@ public class G10HM3 {
                 }
 
                 // Compute the weighted sum of squared distances
-                distSum += Math.pow(curMinDist.get(j), 2) * WP.get(j);
+                distSum += curMinDist.get(j) * WP.get(j);
             }
+
+            // Initialize the probability distribution
+            probDist = new double[P.size()];
+            for (int j = 0; j < P.size(); j++) {
+                probDist[j] = WP.get(j) * curMinDist.get(j) / distSum;
+            }
+
+            // Draw a point according to the new-found probability distribution
+            randNum = rand.nextDouble();
+            double leftSum = 0;
+            double rightSum = probDist[0];
+            for (int j = 1; j <= P.size(); j++) {
+
+                if ((leftSum <= randNum && randNum <= rightSum) || (j == P.size()))
+                {
+                    // A new point is found. Add it to C while removing it from the other arraylists
+                    C.add(P.remove(j-1));
+                    curMinDist.remove(j-1);
+                    WP.remove(j-1);
+                    break;
+                }
+
+                // Update the values of the sums
+                leftSum = rightSum;
+                rightSum += probDist[j];
+            }
+
+        }
+        //_____________________________________________________________________________________________
+
+
+
+        //Refine C with Lloyd's algorithm_______________________________________________________________
+        objFunction = (long) distSum;
+        for(int i=0; i<iter; i++){
 
 
         }
-        //______________________________________________________________________________________________________________
-        return C;
+
+
+        //_____________________________________________________________________________________________
+        return C; //Returns the initial set of C points [RENAME C WITH C_REFINED] !!!!!!!!!!!!!!!!!!!!!
     }
+    //__________________________________________________________________________________________________________________
 
     public static void main(String[] args){
 
